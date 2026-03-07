2bbb181e1b07be6d4ed9be61466175b27659aec2
spencer-vs <isaacharu17@gmail.com>
2026-03-07 13:54:34 +0100

    Deploying to Render

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = 'django-insecure-m3jjir1nq1suj*^61_6(39gd@)z5ua)%libot-$icsx36wtmq7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,6 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_URL = 'login/'
 
